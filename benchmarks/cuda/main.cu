8edc888dba8c76e6779678911469cf9ba886a452
Zaid Qureshi <zaidhorse@gmail.com>
2020-09-14 22:12:45 -0400

    fixed

--- benchmarks/cuda/main.cu
+++ benchmarks/cuda/main.cu
@@ -148,13 +148,16 @@ int main(int argc, char** argv) {
         for (size_t i = 0; i < n_ctrls; i++)
             cuda_err_chk(cudaMemcpy(d_ctrls+i, ctrls[i], sizeof(Controller), cudaMemcpyHostToDevice));
 
-        uint64_t total_cache_size = (4ULL*1024ULL*1024ULL*1024ULL);
-        uint64_t page_size = 128*1024;
-        uint64_t n_pages = total_cache_size/page_size;
 
         uint32_t b_size = 1024;
         uint32_t g_size = 80;
         uint64_t n_threads = b_size * g_size;
+        uint64_t page_size = 128*1024;
+
+        uint64_t total_cache_size = (page_size*n_threads);
+        uint64_t n_pages = total_cache_size/page_size;
+
+        
 
         page_cache_t h_pc(page_size, n_pages, settings, ctrls[0][0]);
 
