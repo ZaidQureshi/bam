8ee9d2f744a368cdafdbda4d2f7af59155e60ddc
Zaid Qureshi <zaidhorse@gmail.com>
2020-09-14 23:59:16 -0400

    remove double

--- benchmarks/cuda/main.cu
+++ benchmarks/cuda/main.cu
@@ -99,8 +99,8 @@ void access_kernel(Controller* ctrls, page_cache_t* pc,  uint32_t req_size, uint
         uint64_t n_blocks = req_size/ ctrls[ctrl].ns.lba_data_size;;
        
         read_data(pc, (ctrls[ctrl].d_qps)+(queue),start_block, n_blocks, tid);
-        __syncthreads();
-        read_data(pc, (ctrls[ctrl].d_qps)+(queue),start_block*2, n_blocks, tid);
+        //__syncthreads();
+        //read_data(pc, (ctrls[ctrl].d_qps)+(queue),start_block*2, n_blocks, tid);
         //printf("tid: %llu finished\n", (unsigned long long) tid);
 
     }
