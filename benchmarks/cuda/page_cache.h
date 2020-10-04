2335431de3962f6e8d6334b1022478790677c036
Zaid Qureshi <zaidhorse@gmail.com>
2020-10-03 23:52:09 -0400

    added

--- benchmarks/cuda/page_cache.h
+++ benchmarks/cuda/page_cache.h
@@ -362,7 +362,7 @@ struct page_cache_t {
         n_ranges_bits = std::log2(max_range);
         n_ranges_mask = max_range-1;
         page_ticket.val = 0;
-
+        page_size_log = std::log2(ps);
         ranges_buf = createBuffer(max_range * sizeof(page_states_t), settings.cudaDevice);
         ranges = (page_states_t*)ranges_buf.get();
         h_ranges = new page_states_t[max_range];
