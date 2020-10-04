381652456b84e4203c4f3bf640fa83feeba524f7
Zaid Qureshi <zaidhorse@gmail.com>
2020-10-03 22:40:47 -0400

    mew

--- benchmarks/cuda/main.cu
+++ benchmarks/cuda/main.cu
@@ -178,7 +178,7 @@ int main(int argc, char** argv) {
         cuda_err_chk(cudaMalloc(&d_ctrls, n_ctrls*sizeof(Controller)));
         for (size_t i = 0; i < n_ctrls; i++)
             cuda_err_chk(cudaMemcpy(d_ctrls+i, ctrls[i], sizeof(Controller), cudaMemcpyHostToDevice));
-        uint64_t b_size = 1;//64;
+        uint64_t b_size = 64;//64;
         uint64_t g_size = 1;//80*16;
         uint64_t n_threads = b_size * g_size;
 
@@ -195,7 +195,7 @@ int main(int argc, char** argv) {
         //QueuePair* d_qp;
         page_cache_t* d_pc = (page_cache_t*) (h_pc.d_pc_ptr);
         #define TYPE uint64_t
-        uint64_t n_elems = 1024;
+        uint64_t n_elems = total_cache_size/2*sizeof(TYPE);
         uint64_t t_size = n_elems * sizeof(TYPE);
 
         range_t<uint64_t> h_range((uint64_t)0, (uint64_t)n_elems, (uint64_t)0, (uint64_t)(t_size/page_size), (uint64_t)0, (uint64_t)page_size, &h_pc, settings);
