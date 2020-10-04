da3ef489cdda69be2c1f05c7dcc23f076551f595
Zaid Qureshi <zaidhorse@gmail.com>
2020-10-03 21:26:41 -0400

    new

--- include/nvm_io.h
+++ include/nvm_io.h
@@ -1,41 +1,5 @@
 #ifndef __NVM_IO_H__
 #define __NVM_IO_H__
-#include "page_cache.h"
-#include "queue.h"
-#include "cuda.h"
-__device__ void read_data(page_cache_t* pc, QueuePair* qp, const uint64_t starting_lba, const uint64_t n_blocks, const unsigned long long pc_entry) {
-    //uint64_t starting_lba = starting_byte >> qp->block_size_log;
-    //uint64_t rem_bytes = starting_byte & qp->block_size_minus_1;
-    //uint64_t end_lba = CEIL((starting_byte+num_bytes), qp->block_size);
-
-    //uint16_t n_blocks = CEIL(num_bytes, qp->block_size, qp->block_size_log);
-
-
-
-    nvm_cmd_t cmd;
-    uint16_t cid = get_cid(&(qp->sq));
-    //printf("cid: %u\n", (unsigned int) cid);
-
-
-    nvm_cmd_header(&cmd, cid, NVM_IO_READ, qp->nvmNamespace);
-    uint64_t prp1 = pc->prp1[pc_entry];
-    uint64_t prp2 = 0;
-    if (pc->prps)
-        prp2 = pc->prp2[pc_entry];
-    //printf("tid: %llu\tstart_lba: %llu\tn_blocks: %llu\tprp1: %p\n", (unsigned long long) threadIdx.x, (unsigned long long) starting_lba, (unsigned long long) n_blocks, (void*) prp1);
-    nvm_cmd_data_ptr(&cmd, prp1, prp2);
-    nvm_cmd_rw_blks(&cmd, starting_lba, n_blocks);
-    uint16_t sq_pos = sq_enqueue(&qp->sq, &cmd);
-
-    uint32_t cq_pos = cq_poll(&qp->cq, cid);
-    sq_dequeue(&qp->sq, sq_pos);
-    cq_dequeue(&qp->cq, cq_pos);
-
-
-    put_cid(&qp->sq, cid);
-
-
-}
 
 
 #endif // __NVM_IO_H__
