966f824da6f458cdcd81093ba98ff161c613267d
Zaid Qureshi <zaidhorse@gmail.com>
2020-11-02 22:34:36 -0500

    polling debug

--- include/nvm_parallel_queue.h
+++ include/nvm_parallel_queue.h
@@ -95,12 +95,12 @@ uint16_t sq_enqueue(nvm_queue_t* sq, nvm_cmd_t* cmd) {
 
     uint64_t k = 0;
     while ((sq->tickets[pos].val.load(simt::memory_order_acquire) != id) || (((pos+1) & sq->qs_minus_1) == (sq->head.load(simt::memory_order_acquire) & (sq->qs_minus_1)))) {
-        if (k++ % 100 == 0)   {
+        /*if (k++ % 100 == 0)   {
             printf("tid: %llu\tpos: %llu\tticket: %llu\tid: %llu\ttickets_pos: %llu\tqueue_head: %llu\tqueue_tail: %llu\n",
                    (unsigned long long) threadIdx.x, (unsigned long long) pos,
                    (unsigned long long)ticket, (unsigned long long)id, (unsigned long long) (sq->tickets[pos].val.load(simt::memory_order_acquire)),
                    (unsigned long long)(sq->head.load(simt::memory_order_acquire) & (sq->qs_minus_1)), (unsigned long long)(sq->tail.load(simt::memory_order_acquire) & (sq->qs_minus_1)));
-        }
+        }*/
         __nanosleep(100);
     }
 
@@ -190,11 +190,11 @@ uint32_t cq_poll(nvm_queue_t* cq, uint16_t search_cid) {
             uint32_t cpl_entry = ((volatile nvm_cpl_t*)cq->vaddr)[loc].dword[3];
             uint32_t cid = (cpl_entry & 0x0000ffff);
             bool phase = (cpl_entry & 0x00010000) >> 16;
-            /* if (j % 100 == 0) */
-            /*     printf("qs_log2: %llu\thead_shift: %llu\thead_shift_and: %llu\thead_shift_and_flipped: %llu\thead: %llu\tcur_head: %llu\tsearch_cid: %llu\tsearch_phase: %llu\tcq->loc: %p\tcq->qs: %llu\ti: %llu\tj: %llu\tcid: %llu\tphase:%llu\n", */
-            /*            (unsigned long long) cq->qs_log2, (unsigned long long) (head >> cq->qs_log2), (unsigned long long) ((head >> cq->qs_log2) & 0x01), (unsigned long long) ~((head >> cq->qs_log2) & 0x01), */
-            /*            (unsigned long long)head, (unsigned long long) cur_head, (unsigned long long) search_cid, (unsigned long long) search_phase, ((volatile nvm_cpl_t*)cq->vaddr)+loc, */
-            /*            (unsigned long long) cq->qs, (unsigned long long) i, (unsigned long long) j, (unsigned long long) cid, (unsigned long long) phase); */
+            if (j % 100 == 0)
+                printf("qs_log2: %llu\thead_shift: %llu\thead_shift_and: %llu\thead_shift_and_flipped: %llu\thead: %llu\tcur_head: %llu\tsearch_cid: %llu\tsearch_phase: %llu\tcq->loc: %p\tcq->qs: %llu\ti: %llu\tj: %llu\tcid: %llu\tphase:%llu\n",
+                       (unsigned long long) cq->qs_log2, (unsigned long long) (head >> cq->qs_log2), (unsigned long long) ((head >> cq->qs_log2) & 0x01), (unsigned long long) ~((head >> cq->qs_log2) & 0x01),
+                       (unsigned long long)head, (unsigned long long) cur_head, (unsigned long long) search_cid, (unsigned long long) search_phase, ((volatile nvm_cpl_t*)cq->vaddr)+loc,
+                       (unsigned long long) cq->qs, (unsigned long long) i, (unsigned long long) j, (unsigned long long) cid, (unsigned long long) phase);
             if ((cid == search_cid) && (phase == search_phase)){
                 /* if ((cpl_entry >> 17) != 0) */
                 /*     printf("NVM Error!!!\n"); */
