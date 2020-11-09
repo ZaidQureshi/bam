5ef7b8e499e574fae0febd2d9fbf711aaea0252f
Zaid Qureshi <zaidhorse@gmail.com>
2020-11-09 04:07:11 -0500

    fixed

--- include/nvm_parallel_queue.h
+++ include/nvm_parallel_queue.h
@@ -100,18 +100,18 @@ uint32_t move_head_sq(nvm_queue_t* q, uint32_t in_cur_head, bool is_sq) {
 __device__
 uint16_t sq_enqueue(nvm_queue_t* sq, nvm_cmd_t* cmd) {
 
-    uint32_t mask = __activemask();
-    uint32_t active_count = __popc(mask);
-    uint32_t leader = __ffs(mask) - 1;
-    uint32_t lane = lane_id();
+    /* uint32_t mask = __activemask(); */
+    /* uint32_t active_count = __popc(mask); */
+    /* uint32_t leader = __ffs(mask) - 1; */
+    /* uint32_t lane = lane_id(); */
     uint32_t ticket;
-    //ticket = sq->in_ticket.fetch_add(1, simt::memory_order_acquire);
-    if (lane == leader) {
-        ticket = sq->in_ticket.fetch_add(active_count, simt::memory_order_acquire);
-    }
+    ticket = sq->in_ticket.fetch_add(1, simt::memory_order_acquire);
+    /* if (lane == leader) { */
+    /*     ticket = sq->in_ticket.fetch_add(active_count, simt::memory_order_acquire); */
+    /* } */
 
-    ticket = __shfl_sync(mask, ticket, leader);
-    ticket += __popc(mask & ((1 << lane) - 1));
+    /* ticket = __shfl_sync(mask, ticket, leader); */
+    /* ticket += __popc(mask & ((1 << lane) - 1)); */
 
     uint32_t pos = ticket & (sq->qs_minus_1);
     uint64_t id = get_id(ticket, sq->qs_log2);
