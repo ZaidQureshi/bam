c0d38ea61c85eaa44fae75999facefda8cded260
Zaid Qureshi <zaidhorse@gmail.com>
2020-11-09 06:26:29 -0500

    added more organization to script

--- benchmarks/cuda/benchmark.sh
+++ benchmarks/cuda/benchmark.sh
@@ -7,10 +7,11 @@ R=1
 B=1024
 for C in 1 2 3 4 5 6 7
 do
-    echo "++++++++++++++++++ $C Controllers ++++++++++++++++++\n"
+    echo "++++++++++++++++++ $C Controllers ++++++++++++++++++"
     for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144
     do
-        ../../build/bin/nvm-cuda-bench --threads=$T --blk_size=$B --reqs=$R --pages=$T --queue_depth=1024 --num_queues=128 --page_size=$P --n_ctrls=$C | grep "IOPs"
+        echo "------------------ $T Threads ------------------"
+        ../../build/bin/nvm-cuda-bench --threads=$T --blk_size=$B --reqs=$R --pages=$T --queue_depth=1024 --num_queues=128 --page_size=$P --n_ctrls=$C | grep "IO"
     done
 
 done
