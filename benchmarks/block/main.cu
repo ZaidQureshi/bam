66b7609d4ecf5587ecffe96fac026425c3f21db0
Zaid Qureshi <zaidhorse@gmail.com>
2020-11-18 22:38:56 -0500

    removed bug

--- benchmarks/block/main.cu
+++ benchmarks/block/main.cu
@@ -205,7 +205,7 @@ int main(int argc, char** argv) {
         page_cache_t* d_pc = (page_cache_t*) (h_pc.d_pc_ptr);
         #define TYPE uint64_t
         uint64_t n_blocks = settings.numBlks;
-        uint64_t t_size = n_elems * sizeof(TYPE);
+        //uint64_t t_size = n_blocks * sizeof(TYPE);
 
         // range_t<uint64_t> h_range((uint64_t)0, (uint64_t)n_elems, (uint64_t)0, (uint64_t)(t_size/page_size), (uint64_t)0, (uint64_t)page_size, &h_pc, settings.cudaDevice);
         // range_t<uint64_t>* d_range = (range_t<uint64_t>*) h_range.d_range_ptr;
