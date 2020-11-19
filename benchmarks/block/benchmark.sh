5877e7f54a22e232cfd4af59079a9c0d5926e920
Zaid Qureshi <zaidhorse@gmail.com>
2020-11-19 14:43:07 -0500

    fixed script

--- benchmarks/block/benchmark.sh
+++ benchmarks/block/benchmark.sh
@@ -15,7 +15,7 @@ do
     for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144
     do
         echo "------------------ $T Threads ------------------"
-        ../../build/bin/nvm-cuda-bench --threads=$T --blk_size=$B --reqs=$R --pages=$T --queue_depth=1024 --num_queues=128 --page_size=$P --n_ctrls=$C --gpu=$G --access_type=$A --ratio=$RT --num_blks=$B --random=$R | grep "Ops"
+        ../../build/bin/nvm-block-bench --threads=$T --blk_size=$B --reqs=$R --pages=$T --queue_depth=1024 --num_queues=128 --page_size=$P --n_ctrls=$C --gpu=$G --access_type=$A --ratio=$RT --num_blks=$B --random=$R | grep "Ops"
     done
 
 done
