8335451afa81ab8c4e81a4c5f9ef9c29e7752799
msharmavikram <vsm2@illinois.edu>
2021-01-06 16:37:48 -0500

    added unbind script

---
+++ scripts/unbind.sh
@@ -0,0 +1,9 @@
+echo -n "0000:e2:00.0" > /sys/bus/pci/devices/0000\:e2\:00.0/driver/unbind
+#echo -n "0000:e3:00.0" > /sys/bus/pci/devices/0000\:e3\:00.0/driver/unbind
+echo -n "0000:e4:00.0" > /sys/bus/pci/devices/0000\:e4\:00.0/driver/unbind
+echo -n "0000:e5:00.0" > /sys/bus/pci/devices/0000\:e5\:00.0/driver/unbind
+echo -n "0000:e6:00.0" > /sys/bus/pci/devices/0000\:e6\:00.0/driver/unbind
+echo -n "0000:e7:00.0" > /sys/bus/pci/devices/0000\:e7\:00.0/driver/unbind
+echo -n "0000:e8:00.0" > /sys/bus/pci/devices/0000\:e8\:00.0/driver/unbind
+echo -n "0000:e9:00.0" > /sys/bus/pci/devices/0000\:e9\:00.0/driver/unbind
+
