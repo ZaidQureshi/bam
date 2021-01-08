1f7723a02e86ca8d5845abe20816dd95b8b87b4b
Zaid Qureshi <zaidhorse@gmail.com>
2021-01-07 23:14:32 -0500

    added markers

--- scripts/bfs_run_nvme_scaling.sh
+++ scripts/bfs_run_nvme_scaling.sh
@@ -1,3 +1,4 @@
+echo "==============================================="
 echo "Running GAP-urand with GPU 0 and Page Size 8192"
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/GAP-urand.bel       -l $((1024*1024*1024*64))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 1 -p 8192 --gpu 0 --threads 64
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/GAP-urand.bel       -l $((1024*1024*1024*64))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 2 -p 8192 --gpu 0 --threads 64
@@ -8,6 +9,7 @@ echo "Running GAP-urand with GPU 0 and Page Size 8192"
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/GAP-urand.bel       -l $((1024*1024*1024*64))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 7 -p 8192 --gpu 0 --threads 64
 
 
+echo "==============================================="
 echo "Running GAP-urand with GPU 5 and Page Size 8192"
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/GAP-urand.bel       -l $((1024*1024*1024*64))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 1 -p 8192 --gpu 5 --threads 64
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/GAP-urand.bel       -l $((1024*1024*1024*64))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 2 -p 8192 --gpu 5 --threads 64
@@ -18,7 +20,7 @@ echo "Running GAP-urand with GPU 5 and Page Size 8192"
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/GAP-urand.bel       -l $((1024*1024*1024*64))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 7 -p 8192 --gpu 5 --threads 64
 
 
-
+echo "==============================================="
 echo "Running uk-2007-05 with GPU 0 and Page Size 8192"
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/uk-2007-05.bel      -l $((1024*1024*1024*320))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 1 -p 8192 --gpu 0 --threads 64
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/uk-2007-05.bel      -l $((1024*1024*1024*320))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 2 -p 8192 --gpu 0 --threads 64
@@ -29,6 +31,7 @@ echo "Running uk-2007-05 with GPU 0 and Page Size 8192"
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/uk-2007-05.bel      -l $((1024*1024*1024*320))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 7 -p 8192 --gpu 0 --threads 64
 
 
+echo "==============================================="
 echo "Running uk-2007-05 with GPU 5 and Page Size 8192"
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/uk-2007-05.bel      -l $((1024*1024*1024*320))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 1 -p 8192 --gpu 5 --threads 64
 ../build/bin/nvm-bfs-bench -f /nvme0/graphs/EMOGI/uk-2007-05.bel      -l $((1024*1024*1024*320))  --impl_type 3 --memalloc 6 --repeat 32  --n_ctrls 2 -p 8192 --gpu 5 --threads 64
