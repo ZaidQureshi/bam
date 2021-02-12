55ef6a6b3a4e15f56f41afb29f634de7173d3be7
Zaid Qureshi <zaidhorse@gmail.com>
2021-02-12 16:46:27 -0500

    fixed error checking

--- include/buffer.h
+++ include/buffer.h
@@ -156,7 +156,7 @@ DmaPtr createDma(const nvm_ctrl_t* ctrl, size_t size)
 
     int err  = posix_memalign(&buffer, 4096, size);
 
-    if (!err) {
+    if (err) {
         throw error(string("Failed to allocate host memory: ") + std::to_string(err));
     }
     int status = nvm_dma_map_host(&dma, ctrl, buffer, size);
