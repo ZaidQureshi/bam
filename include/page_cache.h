cfcfa90bec52e1e02d8dcfe8547c4832e2324be5
Zaid Qureshi <zaidhorse@gmail.com>
2021-03-15 21:27:17 -0400

    removed unecessary hex dump

--- include/page_cache.h
+++ include/page_cache.h
@@ -1114,7 +1114,7 @@ inline __device__ void write_data(page_cache_d_t* pc, QueuePair* qp, const uint6
     nvm_cmd_t cmd;
     uint16_t cid = get_cid(&(qp->sq));
     //printf("cid: %u\n", (unsigned int) cid);
-    printf("startinglba: %llu\tn_blocks: %llu\tpc_entry: %llu\tdata[0]: %llu\n", (unsigned long long) starting_lba, (unsigned long long) n_blocks, pc_entry,
+    printf("write_data startinglba: %llu\tn_blocks: %llu\tpc_entry: %llu\tdata[0]: %llu\n", (unsigned long long) starting_lba, (unsigned long long) n_blocks, pc_entry,
            (unsigned long long) (((unsigned*)(pc->base_addr + (pc_entry*pc->page_size)))[0]));
 
     nvm_cmd_header(&cmd, cid, NVM_IO_WRITE, qp->nvmNamespace);
