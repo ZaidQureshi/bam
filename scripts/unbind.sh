dc3d030e79e34a4465ccfb144c36a466b71d17b5
msharmavikram <msharmavikram@gmail.com>
2021-04-04 10:44:19 -0500

    single liner script that unbinds all the nvme devices that is loaded in the kernel.

--- scripts/unbind.sh
+++ scripts/unbind.sh
@@ -1,9 +1,5 @@
-echo -n "0000:e2:00.0" > /sys/bus/pci/devices/0000\:e2\:00.0/driver/unbind
-#echo -n "0000:e3:00.0" > /sys/bus/pci/devices/0000\:e3\:00.0/driver/unbind
-echo -n "0000:e4:00.0" > /sys/bus/pci/devices/0000\:e4\:00.0/driver/unbind
-echo -n "0000:e5:00.0" > /sys/bus/pci/devices/0000\:e5\:00.0/driver/unbind
-echo -n "0000:e6:00.0" > /sys/bus/pci/devices/0000\:e6\:00.0/driver/unbind
-echo -n "0000:e7:00.0" > /sys/bus/pci/devices/0000\:e7\:00.0/driver/unbind
-echo -n "0000:e8:00.0" > /sys/bus/pci/devices/0000\:e8\:00.0/driver/unbind
-echo -n "0000:e9:00.0" > /sys/bus/pci/devices/0000\:e9\:00.0/driver/unbind
-
+for a in $(dmesg|grep "nvme.*pci"|awk '{print $7}'); 
+do 
+	echo $a
+	echo -n $a > /sys/bus/pci/devices/$a/driver/unbind; 
+done 
