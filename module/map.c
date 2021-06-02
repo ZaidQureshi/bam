b38fc80d7c9e564bee5c2932878739345a237df5
Zaid Qureshi <zaidhorse@gmail.com>
2021-06-02 00:36:57 -0400

    added more debug

--- module/map.c
+++ module/map.c
@@ -177,6 +177,8 @@ static long map_user_pages(struct map* map)
             printk(KERN_ERR "Failed to map page for some reason\n");
             return retval;
         }
+        printk("map_user_page: device: %02x:%02x.%1x\tvaddr: %p\ti: %lu\tdma_addr: %llx\n", map->pdev->bus->number, PCI_SLOT(map->pdev->devfn), PCI_FUNC(map->pdev->devfn),
+            i, map->addrs[i]);
     }
 
     return 0;
