ef091dc3a29eb8443edc3ef1c17699cb2418ed80
Zaid Qureshi <zaidhorse@gmail.com>
2021-07-30 14:57:16 -0500

    added cmd opt for gpuid for benchmark script

--- benchmarks/block/benchmark.sh
+++ benchmarks/block/benchmark.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 set -x
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-	echo Usage $0 numssd  && exit 1
+	echo Usage $0 numssd gpuid  && exit 1
 fi
 
 set -eux
 P=512
 R=0
 B=64
-G=1
+G=$2
 A=0
 RT=50
 NB=$((128*1024*1024))
 CTRLS=$1
-for C in 1 .. $CTRLS
+for ((C=1; C<=$CTRLS; C++))
 do
     echo "++++++++++++++++++ $C Controllers ++++++++++++++++++"
     for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288
