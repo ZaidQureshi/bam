ccb204bb5f54496675e7381369c7c25ded0c93b1
Zaid Qureshi <zaidhorse@gmail.com>
2021-07-30 13:56:42 -0500

    added more threads to benchmark script

--- benchmarks/block/benchmark.sh
+++ benchmarks/block/benchmark.sh
@@ -18,7 +18,7 @@ CTRLS=$1
 for C in 1 .. $CTRLS
 do
     echo "++++++++++++++++++ $C Controllers ++++++++++++++++++"
-    for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144
+    for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288
     do
         echo "------------------ $T Threads ------------------"
         ../../build/bin/nvm-block-bench --threads=$T --blk_size=$B --reqs=1 --pages=$T --queue_depth=1024 --num_queues=128 --page_size=$P --n_ctrls=$C --gpu=$G --num_blks=$NB --access_type=$R
