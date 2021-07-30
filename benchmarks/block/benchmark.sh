46692f6ed2f5d15a3aa10d38d19990412df762b0
Zaid Qureshi <zaidhorse@gmail.com>
2021-07-30 13:50:00 -0500

    updated benchmark script

--- benchmarks/block/benchmark.sh
+++ benchmarks/block/benchmark.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-[ $# -ne 1 ] && echo Usage $0 numssd  && exit 1
+set -x
+
+if [ $# -ne 1 ]
+then
+	echo Usage $0 numssd  && exit 1
+fi
 
 set -eux
 P=512
@@ -9,7 +14,8 @@ G=1
 A=0
 RT=50
 NB=$((128*1024*1024))
-for C in $1
+CTRLS=$1
+for C in 1 .. $CTRLS
 do
     echo "++++++++++++++++++ $C Controllers ++++++++++++++++++"
     for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144
