1243cbf8c3010aa0a98d430a5dded8fca9221d1c
msharmavikram <msharmavikram@gmail.com>
2021-10-13 00:13:26 -0500

    scripts to execute bafs and emogi evaluation

--- benchmarks/cc/run_cc.sh
+++ benchmarks/cc/run_cc.sh
@@ -7,38 +7,26 @@ then
 fi
 
 
-#Initialize set of files are taken from EMOGI and graphBIG.
-
-NUMDATASET=6
+NUMDATASET=4
 declare -a GraphFileArray=(
 "/home/vsm2/bafsdata/GAP-kron.bel"
 "/home/vsm2/bafsdata/GAP-urand.bel"
 "/home/vsm2/bafsdata/com-Friendster.bel"
 "/home/vsm2/bafsdata/MOLIERE_2016.bel"
-"/home/vsm2/bafsdata/uk-2007-05.bel"
-"/home/vsm2/bafsdata/sk-2005.bel"
 )
+# directed graphs and cant do strong component
+#"/home/vsm2/bafsdata/uk-2007-05.bel"
+#"/home/vsm2/bafsdata/sk-2005.bel"
+#"$((1024*1024*1024*320))"
+#"$((1024*1024*1024*384))"
 declare -a GraphFileOffset=(
 "$((1024*1024*1024*0))"
 "$((1024*1024*1024*64))"
 "$((1024*1024*1024*160))"
 "$((1024*1024*1024*224))"
-"$((1024*1024*1024*320))"
-"$((1024*1024*1024*384))"
 )
 
 #echo "${GraphFileArray[5]} offset is ${GraphFileOffset[5]}"
-
-declare -a GraphRootNode=(
-"58720242"
-"58720256"
-"28703654"
-"13229860"
-"46329738"
-"37977096"
-)
-
-
 
 
 CTRL=$1
@@ -49,7 +37,7 @@ TB=128
 for ((gfid=0; gfid<NUMDATASET; gfid++))
 do
     echo "++++++++++++++++++ ${GraphFileArray[gfid]} located at offset ${GraphFileOffset[gfid]} ++++++++++++++++++"
-    for IMPLTYPE in 4 9 #3 4    ##baseline, coalesced, frontier, frontier coaslesced.
+    for IMPLTYPE in 4 10 #3 9    ##baseline, coalesced, hash, hash coalesced.
     do
         echo "++++++++++++++++++ $IMPLTYPE Type ++++++++++++++++++"
         for ((C=1; C<=$CTRL; C++))
@@ -59,7 +47,7 @@ do
             #for P in 512 1024 2048 4096 8192 16384
             do
                 echo "++++++++++++++++++ $P Page size ++++++++++++++++++"
-                ./bin/nvm-bfs-bench -f ${GraphFileArray[gfid]} -l ${GraphFileOffset[gfid]} --impl_type $IMPLTYPE --memalloc $MEMTYPE --src ${GraphRootNode[gfid]} --n_ctrls $C -p $P --gpu $GPU --threads $TB
+                ./bin/nvm-cc-bench -f ${GraphFileArray[gfid]} -l ${GraphFileOffset[gfid]} --impl_type $IMPLTYPE --memalloc $MEMTYPE --n_ctrls $C -p $P --gpu $GPU --threads $TB
             done
         done
     done
