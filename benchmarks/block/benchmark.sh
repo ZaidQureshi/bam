080daa0fb2b9188e59928ab1aa3a5de25998110d
msharmavikram <msharmavikram@gmail.com>
2021-10-13 00:17:36 -0500

    block bench script added to scale linearly upto 35M IOPs in Optane SSD

--- benchmarks/block/benchmark.sh
+++ benchmarks/block/benchmark.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -x
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-	echo Usage $0 numssd gpuid  && exit 1
+	echo Usage $0 numssd gpuid tbsize && exit 1
 fi
 
 set -eux
 P=512
 R=0
-B=64
+B=$3
 G=$2
 A=0
 RT=50
@@ -18,10 +18,10 @@ CTRLS=$1
 for ((C=1; C<=$CTRLS; C++))
 do
     echo "++++++++++++++++++ $C Controllers ++++++++++++++++++"
-    for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288
+    for T in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304
     do
         echo "------------------ $T Threads ------------------"
-        ../../build/bin/nvm-block-bench --threads=$T --blk_size=$B --reqs=1 --pages=$T --queue_depth=1024 --num_queues=128 --page_size=$P --n_ctrls=$C --gpu=$G --num_blks=$NB --access_type=$R
+        ../../build/bin/nvm-block-bench --threads=$T --blk_size=$B --reqs=1 --pages=$T --queue_depth=1024 --num_queues=135 --page_size=$P --n_ctrls=$C --gpu=$G --num_blks=$NB --access_type=$R
     done
 
 done
