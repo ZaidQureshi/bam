bb78c5375f7ef76c9e54bca9e020dc8b7adbcaab
msharmavikram <vsm2@illinois.edu>
2022-04-26 11:15:55 -0500

    update

--- benchmarks/bfs/settings.h
+++ benchmarks/bfs/settings.h
@@ -46,6 +46,7 @@ struct Settings
     uint32_t        bus;
     uint32_t        devfn;
     uint64_t stride;
+    uint64_t coarse;
     uint32_t n_ctrls;
     size_t blkSize;
     size_t queueDepth;
@@ -383,6 +384,7 @@ void Settings::parseArguments(int argc, char** argv)
         {'q', OptionPtr(new Range(numQueues, 1, 65536, "num_queues", "number of queues per controller", "1"))},
         {'M', OptionPtr(new Option<uint64_t>(maxPageCacheSize, "number", "maxPCSize", "Maximum Page Cache size in bytes", "8589934592"))},
         {'P', OptionPtr(new Option<uint64_t>(stride, "number", "STRIDE", "Hashing stride factor for bfs coal. It is calculated as P = stride. Assumes power of 2", "1"))},
+        {'C', OptionPtr(new Option<uint64_t>(coarse, "number", "COARSE", "Thread coarsening factor", "1"))},
 //        {'e', OptionPtr(new Range(numElems, 1, (uint64_t)std::numeric_limits<uint64_t>::max, "num_elems", "number of 64-bit elements in backing array", "2147483648"))},
     };
 
@@ -483,6 +485,7 @@ Settings::Settings()
     repeat = 32;
     src = 0;
     stride = 1;
+    coarse = 1;
     maxPageCacheSize = 8589934592;
 }
 
